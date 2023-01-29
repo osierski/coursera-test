68a1e787660eb09e4dfb9776e45446bd4213c8cf
Patryk Osierski <patryk.lozinski@gmail.com>
2023-01-29 23:00:07 +0100

    solution 1

---
+++ module1-solution/app.js
@@ -0,0 +1,42 @@
+(function () {
+    'use strickt';
+
+    var maxLunchMates = 3;
+    var messageOk = "Enjoy!";
+    var messageNotOk = "Too much!";
+    var messageNoMates = "C'mon, provide some mates";
+
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope', '$filter']; //$filter is not neccecery here 
+    function LunchCheckController($scope, $filter) {
+
+        $scope.checkResult = "";
+
+
+        $scope.doCheck = function () {
+            var mates = $scope.names.split(',');
+
+            var matesCount = 0;
+            for (var i = 0; i < mates.length; i++) {
+                console.log('-' + mates[i].trim() + '-; ');
+                if (mates[i].trim().length > 0) {
+                    matesCount++;
+                }
+            }
+
+            console.log('--' + matesCount + '-- ');
+            if (matesCount = 0) {
+                $scope.checkResult = messageNoMates;
+            } else {
+                if (matesCount <= maxLunchMates) {
+                    $scope.checkResult = messageOk;
+                } else {
+                    $scope.checkResult = messageNotOk;
+                }
+            }
+        };
+
+    }
+})();
